9d6f75775d1c86aae1f8a9c4819b913b62d916e7
BarraudRm <remi.barraud@etu.minesparis.psl.eu>
2025-10-01 11:19:40 +0200

    quesiton 2.4

---
+++ BUFFERS/buffer_Remi.py
@@ -0,0 +1,19 @@
+# QUESTION 2.4
+
+Dict_Param = {
+    "K": K_ref,
+    "lambda_s": lambda_s_ref,
+    "n": n_ref
+}
+
+def Val_model (Dict_Param, brouillee = True):
+    ref = rivBed.runForwardModelSteadyState(Dict_Param["K"], Dict_Param["lambda_s"], Dict_Param["n"])
+    t_echant = get_temp_echant(echant, brouillee=brouillee)
+    return ref, t_echant
+
+ref, t_echant_brouille = Val_model(Dict_Param)
+
+#print(max(t_echant_brouille))
+#print(ref)
+
+#FIN QUESTION 2.4
