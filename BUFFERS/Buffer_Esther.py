f4e092fb7e5e764ea9c344898e517332a4715d9c
Esther Pautrel <esther.pautrel@etu.minesparis.psl.eu>
2025-10-01 12:18:27 +0200

    Q.4.1

--- BUFFERS/Buffer_Esther.py
+++ BUFFERS/Buffer_Esther.py
@@ -12,10 +12,44 @@ def perturbation(param, sigma, range, K=False):
         result = 10**(-result)
     return result
 
-""""""
+#Q.4.1 
+
+%pylab inline
+
+from codepyheat.geometry import Column
+from codepyheat import JSONPATH
+
+K_ref = 1e-5
+lambda_s_ref = 2
+n_ref = 0.1
+
+# step 1
+dico = {
+    "depth": {
+            "val": "1",
+            "unit": "m"
+        },
+    "ncells": 100
+}
+
+rivBed = Column(dico)
+rivBed.printProps()
+
+# step 2
+rivBed.setHomogeneousPorMed(JSONPATH + "paramColumn.json")
+rivBed.physProp.printProps()
+
+# step 3
+rivBed.setBcHyd(JSONPATH + "configBcHydro_4.1.json")
+rivBed.setBcT(JSONPATH + "configBcTemp_4.1.json")
+
+# step 4 running forward model with other parameter values
+ref = rivBed.runForwardModelSteadyState(K_ref, lambda_s_ref, n_ref)
 
 
-"""""""
+
+###Ne pas prendre en compte ce qui est en-dessous!!!###
+
 #Q.2.4 cellule 2
 
 Dict_Param = {
