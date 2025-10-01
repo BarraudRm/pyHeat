cbcbddcb65038381f0bf2c84525389c49f5c7bc8
Esther Pautrel <esther.pautrel@etu.minesparis.psl.eu>
2025-10-01 16:09:58 +0200

    réflexion sur Q.4.1 + idée Q.4.3

--- BUFFERS/Buffer_Esther.py
+++ BUFFERS/Buffer_Esther.py
@@ -31,3 +31,28 @@ rivBed.setBcT(JSONPATH + "configBcTemp_4.1.json")
 
 # step 4 running forward model with other parameter values
 ref = rivBed.runForwardModelSteadyState(K_ref, lambda_s_ref, n_ref)
+
+
+#suite Q.4.1 en réflexion... j'essaye de voir comment accéder aux graphiques des différentes simulations... 
+#à partir du code de geometry.py... 
+
+simulations = []
+
+for elt in params:
+    Dict_Param["moinslog10K"] = elt[0]
+    Dict_Param["lambda_s"] = elt[1]
+    Dict_Param["n"] = elt[2]
+    simulations.append(model_direct(Dict_Param, brouillee = True))
+    
+#it = caracItSteadyTemplate.format(
+#               physP.getUpperK(), physP.getLambda(), physP.getPorosity()
+#            )
+
+for simu in simulations:
+    simu.iterativePlotT(it) #essayer de le mettre sur un même graphique...
+
+
+
+#Q.4.3 
+
+#idée potentielle : cru/innondations, statue au milieu de la Seine - le zouave du point de l'Alma 
